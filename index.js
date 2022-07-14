274c01c689b68569ea214db70d7183a6a586a877
Doug Pfeffer <doug.pfeffer@gmail.com>
2022-07-14 13:08:33 -0400

    Added Sentry for error catching

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // This is the handler for Lambda.
 // Processes on Guild per run.
 
+const Sentry = require("@sentry/serverless");
 const GuildIterator = require('./src/GuildIterator.js')
 
-exports.handler = async (event) => {
+Sentry.AWSLambda.init({
+  dsn: process.env.SENTRY_URL,
+  tracesSampleRate: 1.0
+});
+
+exports.handler = Sentry.AWSLambda.wrapHandler(async (event) => {
   const gi = new GuildIterator('manifest.json')
   await gi.fetchManifest()
   await gi.processNextGuild()
-}
+})
